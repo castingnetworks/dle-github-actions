e7bf9190cceb1117f3ecbae8b9e614c57f796c14
mcfearsome <jesse@mcfearsome.dev>
2023-10-20 12:13:29 -0400

    chore; debug

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -36,7 +36,7 @@ echo $JSON_DATA > migration_data.json
 
 cat migration_data.json
 
-response_code=$(curl --show-error --silent --location --request POST "${DLMC_CI_ENDPOINT}/migration/run" --write-out "%{http_code}" \
+response_code=$(curl -vvv --location --request POST "${DLMC_CI_ENDPOINT}/migration/run" \
 --header "Verification-Token: ${DLMC_VERIFICATION_TOKEN}" \
 --header 'Content-Type: application/json' \
 --output response.json \
